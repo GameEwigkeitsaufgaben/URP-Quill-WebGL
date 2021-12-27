f71bb0ac4049226a184e08a0d240cf61f4954742
smeerws <hello@smeerws.at>
2021-12-27 13:35:44 +0100

    Quill shader

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -1,23 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
 public class Manager : MonoBehaviour
 {
     public VideoPlayer player;
-    public VideoClip clip;
 
     private void Start()
     {
-        Debug.Log("clipname " + "testvideo.mp4");
         player.url = System.IO.Path.Combine(Application.streamingAssetsPath, "testvideo.mp4");
-        Debug.Log("url " + player.url);
     }
 
     public void PlayTheVideo()
     {
-      
         player.Play();
     }
 }
